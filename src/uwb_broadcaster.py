922b900a65018c41e84056c8edd06ee0fc168450
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-01-17 18:50:40 +0100

    [TASK] Add service endpoint to set real position

--- src/uwb_broadcaster.py
+++ src/uwb_broadcaster.py
@@ -6,22 +6,24 @@ from rclpy.node import Node
 
 from tf2_ros import TransformBroadcaster
 
-from ros2_uwb_simulation.srv import UwbPosition
+from ros2_uwb_simulation.srv import UwbPosition, RealPosition
 
 class UwbBroadcaster(Node):
 
     def __init__(self, node_name = 'uwb_broadcaster'):
         super().__init__(node_name)
         self.declare_parameter('translation', [0.0, 0.0, 0.0])
+        self.real_position = self.get_parameter('translation').get_parameter_value().double_array_value
 
         self.tf_broadcaster = TransformBroadcaster(self)
         self.position_srv = self.create_service(UwbPosition, self.get_name()+'/get_position', self.position_callback, callback_group=rclpy.callback_groups.ReentrantCallbackGroup())
+        self.set_real_position_srv = self.create_service(RealPosition, self.get_name()+'/set_real_position', self.set_real_position_callback, callback_group=rclpy.callback_groups.ReentrantCallbackGroup())
 
         self.timer = self.create_timer(0.1, self.real_position_publish)
         self.uwb_frame_id = 'uwb_'+self.get_name()
 
     def real_position_publish(self):
-        translation = self.get_parameter('translation').get_parameter_value().double_array_value
+        translation = self.real_position
         t = TransformStamped()
 
         t.header.stamp = self.get_clock().now().to_msg()
@@ -39,12 +41,20 @@ class UwbBroadcaster(Node):
         self.tf_broadcaster.sendTransform(t)
 
     def position_callback(self, request, response):
-        translation = self.get_parameter('translation').get_parameter_value().double_array_value
+        translation = self.real_position
 
         response.x = translation[0]
         response.y = translation[1]
         response.z = translation[2]
         response.type = 0
+
+        return response
+
+    def set_real_position_callback(self, request, response):
+
+        self.real_position[0] = request.x
+        self.real_position[1] = request.y
+        self.real_position[2] = request.z
 
         return response
 
