3c0ded3a657a9965135447213a58f52dbfd78990
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2023-01-17 18:32:27 +0100

    [TASK] Fix indentation

--- src/uwb_broadcaster.py
+++ src/uwb_broadcaster.py
@@ -10,17 +10,17 @@ from ros2_uwb_simulation.srv import UwbPosition
 
 class UwbBroadcaster(Node):
 
-   def __init__(self, node_name = 'uwb_broadcaster'):
-       super().__init__(node_name)
-       self.declare_parameter('translation', [0.0, 0.0, 0.0])
+    def __init__(self, node_name = 'uwb_broadcaster'):
+        super().__init__(node_name)
+        self.declare_parameter('translation', [0.0, 0.0, 0.0])
 
-       self.tf_broadcaster = TransformBroadcaster(self)
-       self.position_srv = self.create_service(UwbPosition, self.get_name()+'/get_position', self.position_callback, callback_group=rclpy.callback_groups.ReentrantCallbackGroup())
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.position_srv = self.create_service(UwbPosition, self.get_name()+'/get_position', self.position_callback, callback_group=rclpy.callback_groups.ReentrantCallbackGroup())
 
-       self.timer = self.create_timer(0.1, self.real_position_publish)
-       self.uwb_frame_id = 'uwb_'+self.get_name()
+        self.timer = self.create_timer(0.1, self.real_position_publish)
+        self.uwb_frame_id = 'uwb_'+self.get_name()
 
-   def real_position_publish(self):
+    def real_position_publish(self):
         translation = self.get_parameter('translation').get_parameter_value().double_array_value
         t = TransformStamped()
 
@@ -38,15 +38,15 @@ class UwbBroadcaster(Node):
 
         self.tf_broadcaster.sendTransform(t)
 
-   def position_callback(self, request, response):
-       translation = self.get_parameter('translation').get_parameter_value().double_array_value
+    def position_callback(self, request, response):
+        translation = self.get_parameter('translation').get_parameter_value().double_array_value
 
-       response.x = translation[0]
-       response.y = translation[1]
-       response.z = translation[2]
-       response.type = 0
+        response.x = translation[0]
+        response.y = translation[1]
+        response.z = translation[2]
+        response.type = 0
 
-       return response
+        return response
 
 if __name__ == "__main__":
     rclpy.init()
